6b85283a073235d97cd39ce544550e8db230117c
Pjotters-Chain <05.hek_domein@icloud.com>
2025-03-10 22:30:22 +0100

    Login Update

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,8 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js';
 import { getAnalytics } from "firebase/analytics";
-import { getAuth } from 'firebase/auth';
-import { getDatabase } from 'firebase/database';
+import { getAuth } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js';
+import { getDatabase, ref } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,7 +15,7 @@ const firebaseConfig = {
     projectId: "pjotters-company",
     storageBucket: "pjotters-company.firebasestorage.app",
     messagingSenderId: "64413422793",
-    appId: "1:64413422793:web:37debb74f7c7d3ead6e918",
+    appId: "1:64413422793:web:37debb74f7c7d3eade6918",
     measurementId: "G-BHQ2S1TWTH"
 };
 
